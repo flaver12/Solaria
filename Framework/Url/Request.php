67abec4d508ba20942fbaa3a888f781885e4ed69
flaver <flaverkleiber@yahoo.de>
2017-05-02 21:30:16 +0100

    Some php doc stuff

--- Framework/Url/Request.php
+++ Framework/Url/Request.php
@@ -1,4 +1,12 @@
 <?php
+/**
+*
+* Request wrapper class
+*
+* @author Flavio Kleiber <flaverkleiber@yahoo.de>
+* @package FM\Framework\Url
+* @copyright 2016-2017 Flavio Kleiber
+*/
 
 namespace FM\Framework\Url;
 
@@ -7,6 +15,11 @@ class Request {
     public $url;
     public $arguments;
 
+    /**
+    * Check if the request is a post request
+    *
+    * @return bool
+    */
     public function isPost() {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             return true;
@@ -14,6 +27,13 @@ class Request {
         return false;
     }
 
+    /**
+    * Get the content of the post array
+    * if the value is empty you get the post array
+    *
+    * @param value string
+    * @return mixed array|string
+    */
     public function getPost($value='') {
         return ($value == '') ?  $_POST :  $_POST[$value];
     }
--- Framework/Url/Response.php
+++ Framework/Url/Response.php
@@ -1,4 +1,12 @@
 <?php
+/**
+*
+* Response wrapper class
+*
+* @author Flavio Kleiber <flaverkleiber@yahoo.de>
+* @package FM\Framework\Url
+* @copyright 2016-2017 Flavio Kleiber
+*/
 
 namespace FM\Framework\Url;
 
@@ -6,9 +14,16 @@ use FM\Framework\Url\Url;
 
 class Response {
 
-  public static function redirect($url, $statusCode = 303) {
-      header('Location: '.URL::getBaseURL().'/'. $url, true, $statusCode);
-      die();
-  }
+    /**
+    * Redirect to a url
+    *
+    * @param url string
+    * @param statusCode int default is 303
+    * @return void
+    */
+     public static function redirect($url, $statusCode = 303) {
+         header('Location: '.URL::getBaseURL().'/'. $url, true, $statusCode);
+         die();
+     }
 
 }
